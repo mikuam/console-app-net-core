27e710a9e232f860718b5d696d20b4347c222003
Michał Białecki <mik.bialecki@gmail.com>
2019-03-19 06:02:21 +0100

    [minor] Added validator tests

---
+++ MichalBialecki.com.TicketStore.Console.Tests/Helpers/CommandValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿namespace MichalBialecki.com.TicketStore.Console.Tests.Helpers
+{
+    using MichalBialecki.com.TicketStore.Console.Helpers;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CommandValidatorTests
+    {
+        private CommandValidator _commandValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _commandValidator = new CommandValidator();
+        }
+
+        [TestCase("A1", true)]
+        [TestCase("A15", true)]
+        [TestCase("A11", true)]
+        [TestCase("H15", true)]
+        [TestCase("H16", false)]
+        [TestCase("K15", false)]
+        [TestCase("I4", false)]
+        [TestCase("K.", false)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+        public void IsValid_GivenCommand_ReturnsExpectedResult(string command, bool expectedResult)
+        {
+            // Arrange & Act
+            var result = _commandValidator.IsValid(command);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
